628999db13c331253d651298ec77df2f88807f55
Rita Helwangi <rita.helwangi@gmail.com>
2025-09-25 10:16:49 +0200

    get all products

--- serverSrc/routes/products.ts
+++ serverSrc/routes/products.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import type { Request, Response } from "express";
-import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { GetCommand,ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { db } from "../data/db.js";
 
 const router: Router = express.Router();
@@ -26,6 +26,28 @@ router.get('/:productId', async (req: Request, res: Response) => {
 	} catch (error) {
 		console.error("Error fetching single product:", error);
 		res.status(500).json({ message: 'Could not fetch product', error: String(error) });
+	}
+});
+//all products
+router.get('/', async (req: Request, res: Response) => {
+	try {
+		const result = await db.send(new ScanCommand({
+			TableName: "CandyShop",
+			FilterExpression: "pk = :pk",
+			ExpressionAttributeValues: {
+				":pk": "PRODUCT"
+			}
+		}));
+		
+		if (result.Items) {
+			res.json(result.Items);
+		} else {
+			res.json([]);
+		}
+		
+	} catch (error) {
+		console.error("Error fetching all products:", error);
+		res.status(500).json({ message: 'Could not fetch products', error: String(error) });
 	}
 });
 
